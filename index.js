24bebb0d4fa7c8ac83c5f2cb47cf99eabf2446b4
kelvin <kelvinkimathi087@gmial.com>
2026-07-03 15:39:49 +0300

    fix cors

--- index.js
+++ index.js
@@ -370,6 +370,7 @@ app.use(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://localhost:8080",
+        "https://stk-mpesa-3.onrender.com",
       ];
 
       if (allowedOrigins.indexOf(origin) === -1) {
